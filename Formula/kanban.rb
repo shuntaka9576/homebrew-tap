7b92ad2f19d3548be8723be09dc06719944416d4
shuntaka9576 <shuntaka9576@gmail.com>
2020-05-06 18:41:33 +0900

    Add dsl

---
+++ Formula/kanban.rb
@@ -0,0 +1,12 @@
+class Kanban < Formula
+  name = "kanban"
+  desc ":octocat: Simple terminal UI for GitHub Project"
+  homepage "https://github.com/shuntaka9577/kanban"
+  version "0.1.0"
+  sha256 "3e7628259f67a454e060af72fc9e12ee1cb96da9884b0b2eaae0795526516857"
+  url "https://github.com/shuntaka9576/#{name}/releases/download/v#{version}/#{name}_darwin_x86_64.tar.gz"
+
+  def install
+    bin.install name
+  end
+end
