6ecb596260bf1a9a514dad00442d8c51735821fb
shuntaka9576 <shuntaka9576@gmail.com>
2020-05-21 22:54:25 +0900

    Update kanban to v0.1.3

--- Formula/kanban.rb
+++ Formula/kanban.rb
@@ -2,8 +2,8 @@ class Kanban < Formula
   name = "kanban"
   desc ":octocat: Simple terminal UI for GitHub Project"
   homepage "https://github.com/shuntaka9577/kanban"
-  version "0.1.2"
-  sha256 "b12d949f1d31996eb2dfaa905b24cc7f1ca83a94fcdbd400c14ac3a12fadad2b"
+  version "0.1.3"
+  sha256 "078401d17b351574a3373ad62bd638cbf3014077a9813331ab9565acd135eac3"
   url "https://github.com/shuntaka9576/#{name}/releases/download/v#{version}/#{name}_darwin_x86_64.tar.gz"
 
   def install
