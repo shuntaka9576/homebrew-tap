af68a648c5e315f30672e83a41c82e235df8dbda
shuntaka9576 <shuntaka9576@gmail.com>
2022-05-30 07:43:46 +0900

    Update ddbrew to v0.0.2

--- Formula/ddbrew.rb
+++ Formula/ddbrew.rb
@@ -2,8 +2,8 @@ class Ddbrew < Formula
   name = "ddbrew"
   desc "Simple DynamoDB utility"
   homepage "https://github.com/shuntaka9577/ddbrew"
-  version "0.0.1"
-  sha256 "2024e199ca75f1df8ebd16b3aa0b8b5571d22c2c79b6ac9e88929c10731dec58"
+  version "0.0.2"
+  sha256 "6b816212e580cafc3603e1093639a582ca3fe7fd62dacf6326ce3c38e89d3a66"
   url "https://github.com/shuntaka9576/#{name}/releases/download/v#{version}/#{name}_#{version}_Darwin_amd64.tar.gz"
 
   def install
